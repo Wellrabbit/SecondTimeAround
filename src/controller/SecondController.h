7aea7dc8a7d1d325e095ffccefefa8d7d6b967a2
Emily Orton <eort3611@740s-pl-106.local>
2016-01-19 12:42:11 -0700

    10

---
+++ src/controller/SecondController.h
@@ -0,0 +1,24 @@
+/*
+ * SecondController.h
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: eort3611
+ */
+
+#ifndef CONTROLLER_SECONDCONTROLLER_H_
+#define CONTROLLER_SECONDCONTROLLER_H_
+
+class AfternoonController
+{
+private:
+	int classCount;
+public:
+	AfternoonContoller();
+	void setClassCount(int classCount);
+	int getClassCount();
+	void start();
+};
+
+
+
+#endif /* CONTROLLER_SECONDCONTROLLER_H_ */
